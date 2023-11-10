ce5ecbf92f4f9258dabba8a379e9e6e3cfe983a3
Fernando Feliansyah <fernandofeliansyah_2226250051@mhs.mdp.ac.id>
2023-11-10 18:22:25 +0700

    GridView

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screen/detail_screen.dart';
+import 'package:wisata_candi/screen/home_screen.dart';
 import 'package:wisata_candi/screen/profile_screen.dart';
 import 'package:wisata_candi/screen/search_screen.dart';
 import 'package:wisata_candi/screen/sign_in_screen.dart';
@@ -32,7 +33,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: SearchScreen(),
+      home: HomeScreen(),
+      // home: SearchScreen(),
       // home: ProfileScreen(),
       // home: DetailScreen(candi: candiList[0]),
     );
