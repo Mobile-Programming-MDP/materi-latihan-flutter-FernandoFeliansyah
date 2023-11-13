9348c8e48d16db08c74f1cdc797c7725a7986cac
Fernando Feliansyah <fernandofeliansyah_2226250051@mhs.mdp.ac.id>
2023-11-13 19:07:45 +0700

    P-19

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:wisata_candi/screen/home_screen.dart';
 import 'package:wisata_candi/screen/profile_screen.dart';
 import 'package:wisata_candi/screen/search_screen.dart';
 import 'package:wisata_candi/screen/sign_in_screen.dart';
+import 'package:wisata_candi/screen/favorite_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,10 +34,72 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: MainScreen(),
+      // home: HomeScreen(),
       // home: SearchScreen(),
       // home: ProfileScreen(),
       // home: DetailScreen(candi: candiList[0]),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //TODO 1. Deklarasikan Variabel
+  int _currentIndex = 0;
+  final List<Widget>_children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO 2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple),
+              label: 'Home'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple),
+              label: 'Search'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.deepPurple),
+              label: 'Favorite'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple),
+              label: 'Profile'
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showSelectedLabels: true,
+        ),
+      ),
+      //TODO 3. Buat properti bottomNavigationBar dengan nilai theme
+      //TODO 4. Buat data dan child dari Theme
     );
   }
 }
